8ca1338faee7ded615dba50080050b8ecbeb8c60
pFriesch <p.friesch@posteo.de>
2015-03-23 03:39:00 +0100

    modified email message

--- config.py
+++ config.py
@@ -1,34 +1,22 @@
 # -*- encoding: utf-8 *-*
 
-import datetime
+#import datetime
 
 # customize traveling specs
 # mind that ltur offers bahn tickets only for the next 7 days starting from tomorrow
 from_city = 'Berlin Hbf'
-to_city = 'München Hbf'
+to_city = 'Augsburg Hbf'
 
 # default to tomorrow
-on_date = ( datetime.date.today() + datetime.timedelta( days=1 )).strftime( '%d.%m.%Y' )
-#on_date = '21.01.2013'
+#on_date = ( datetime.date.today() + datetime.timedelta( days=1 )).strftime( '%d.%m.%Y' )
+on_date = '25.03.2015'
 
-at_time = '09:12'
+at_time = '20:12'
 max_price = 40.0
-
-# ltur's Bahn webpage: journey form (dynamically loaded after page load via AJAX)
-user_url = 'http://www.ltur.com/de/bahn.html?omnin=DB-DE'
-scraper_url = 'http://bahn.ltur.com/ltb/searchform/external'
 
 # set the mode of notification: pushover or email
 # MODE = 'pushover'
 MODE = 'email'
-
-# keywords for webscraping
-TRIGGER = [
-    'price_Fernweh_H',      # really cheap prices
-    'price_Sparpreis_H'     # medium cheap prices...
-]
-PRICE_TAG_REGEX = u'([0-9]{1,3}(,|.)?([0-9]{1,2}))?\s*€?'
-
 
 # Pushover config
 APP_TOKEN   = 'EpMD3BrlmxioeKvGujVccccPqHeUxd'
@@ -36,11 +24,14 @@ USER_TOKEN  = ''
 PUSHOVER_URL = "api.pushover.net"
 PUSHOVER_PATH = "/1/messages.json"
 
-
 # E-mail config
-EMAIL = 'you@example.org'
-FROM_EMAIL = 'lturdaemon@example.org'
-SMTP_SERVER = 'smtp.example.org'
-SMTP_USER = 'lturdaemon@example.org'  # optional
-SMTP_PASS = 'somesecretpassword'  # optional
+EMAIL = 'x@gmail.com'
+FROM_EMAIL = 'x@gmail.com'
+#gmail:
+SMTP_SERVER = 'smtp.gmail.com'
+# SMTP_SERVER = 'smtp.example.org'
+SMTP_USER = 'x@gmail.com'  # optional
+SMTP_PASS = 'x'  # optional
+
+
 
--- ltur-notifier.py
+++ ltur-notifier.py
@@ -11,17 +11,36 @@ from bs4 import BeautifulSoup
 
 from config import *
 
+# ltur's Bahn webpagde: journey form (dynamically loaded after page load via AJAX)
+user_url = 'http://www.ltur.com/de/bahn.html?omnin=DB-DE'
+scraper_url = 'http://bahn.ltur.com/ltb/searchform/external'
+#
+# keywords for webscraping
+# TRIGGER = [
+# 'price_Fernweh_H',      # really cheap prices
+# 'price_Sparpreis_H'     # medium cheap prices...
+# ]
+#
+# PRICE_TAG_REGEX = u'([0-9]{1,3}(,|.)?([0-9]{1,2}))?\s*€?'
+
 # TODO: error handling
 # (1) if on_date - today > 7, inform user
 
 def main():
     page = submit_form()
-    prices = parse_page(page.read(), TRIGGER)
-    if any([p <= max_price for p in prices]):
+
+    entries = parse_cheap_entries(page)
+    cheapest = max_price
+
+    for entry in entries:
+        if entry['price'] < cheapest:
+            cheapest = entry['price']
+
+    if cheapest < max_price:
         if MODE == 'pushover':
-            send_pushover(min(prices))
+            send_pushover(cheapest)
         elif MODE == 'email':
-            send_mail(min(prices))
+            send_mail(entries, cheapest)
 
 
 def submit_form():
@@ -46,22 +65,58 @@ def submit_form():
 
     return br.submit()
 
+# Entry Tuple (fromCity,departureTime,toCity,arrivalTime,via[],price)
+def parse_cheap_entries(page):
+    soup = BeautifulSoup(page)
 
-def parse_page(haystack, needles):
-    bs = BeautifulSoup(haystack)
-    gems = []
-    price_tags = []
-    for needle in needles:
-        price_tags.extend(bs.find_all('td', attrs={'class': needle}))
+    entries = soup.find_all('tr', ['even', 'odd'])
 
-    for price_tag in price_tags:
-        price_string = price_tag.get_text().strip()
-        match = re.match(PRICE_TAG_REGEX, unicode(price_string))
-        if match:
-            price = match.group(1)
-            price = re.sub(',', '.', price)
-            gems.append(float(price))
-    return gems
+    def is_sub_class(tag):
+        return 'subinfotop' in tag['class'] or 'subinfobottem' in tag['class']
+
+    def is_cheap_entry(entry):
+        is_cheap = False
+        for elem in entry.find_all('td'):
+            if 'price_Fernweh_H' in elem['class'] or 'price_Sparpreis_H' in elem['class']:
+                is_cheap = True
+        return is_cheap
+
+    filtered_entries = []
+    for entry in entries:
+        if not entry.has_attr('style') and not is_sub_class(entry):
+            if is_cheap_entry(entry):
+                filtered_entries.append(entry)
+
+    beautified_entries = []
+    for entry in filtered_entries:
+        elements = entry.find_all('td')
+        ticket = dict()
+        ticket['departure_date'] = elements[1].text.strip()[0:11]
+        ticket['arrival_date'] = elements[1].text.strip()[11:22]
+        ticket['departure_time'] = elements[2].text[0:5]
+        ticket['arrival_time'] = elements[2].text[5:10]
+        ticket['travel_time'] = elements[3].text
+        ticket['price'] = int(elements[5].text.replace('\n', '').replace(' ', '')[0:2])
+        beautified_entries.append(ticket)
+
+    return beautified_entries
+
+
+# def parse_page(haystack, needles):
+# bs = BeautifulSoup(haystack)
+# gems = []
+#     price_tags = []
+#     for needle in needles:
+#         price_tags.extend(bs.find_all('td', attrs={'class': needle}))
+#
+#     for price_tag in price_tags:
+#         price_string = price_tag.get_text().strip()
+#         match = re.match(PRICE_TAG_REGEX, unicode(price_string))
+#         if match:
+#             price = match.group(1)
+#             price = re.sub(',', '.', price)
+#             gems.append(float(price))
+#     return gems
 
 
 def send_pushover(cheapest):
@@ -82,13 +137,21 @@ def send_pushover(cheapest):
     conn.close()
 
 
-def send_mail(cheapest):
+def send_mail(entries, cheapest):
+    preparedMsg = "Train rides:\n\n"
+    preparedMsg += "departure\tarrival\t\t\t\ttravel time\tprice\n"
+    for entry in entries:
+        preparedMsg += entry['departure_date'] + "\t" + entry['arrival_date'] + "\n" + \
+                       entry['departure_time'] + "\t\t" + entry['arrival_time'] + "\t\t\t\t" + entry['travel_time'] +\
+                       "\t\t" + str(entry['price']) + " Euro\n\n"
+    preparedMsg += "\n" + user_url
+
     import smtplib
     from email.mime.text import MIMEText
 
     # Create a text/plain message
-    msg = MIMEText("Ltur notification. cheapest offer: %s €\n\n%s" % ( str(cheapest), user_url ))
-    msg['Subject'] = 'Ltur notifier: %s ' % str(cheapest)
+    msg = MIMEText(preparedMsg)
+    msg['Subject'] = 'Ltur Notifier ' + str(cheapest) + " Euro"
     msg['From'] = FROM_EMAIL
     msg['To'] = EMAIL
 
